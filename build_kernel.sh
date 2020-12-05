9bd921c0ba61e3916de77d78b081ba9904612769
Svirusx <45526199+Svirusx@users.noreply.github.com>
2020-12-05 22:25:55 +0100

    Update to Samsung source G930FXXS6ESI4

---
+++ build_kernel.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export ARCH=arm64
+export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+
+make exynos8890-herolte_defconfig
+make -j64
